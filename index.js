66215807f2f494b502bba4451857ede9a896aa52
psysecboi <replypkv@gmail.com>
2024-12-11 18:04:38 +0530

    Adding project files

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+let request=0;
+function middleWare(req, res, next){
+    request = request + 1;
+    console.log(request);
+    next();
+}
+
+app.use(middleWare);
+
+app.get("/sum", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
